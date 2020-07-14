d5d80c73dd7dc91ec961af039f554bcdf10da552
edwin716 <65332564+edwin716@users.noreply.github.com>
2020-07-14 19:51:53 +0800

    whynusforum

--- frontend/src/components/contactcard/index.js
+++ frontend/src/components/contactcard/index.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import {Card} from 'semantic-ui-react';
 import logo from './fass.png';
-import {Link} from 'react-router-dom';
+import logo1 from './computing.png';
+import logo2 from './biz.png';
+import logo3 from './med.png';
+import logo4 from './law.png';
+import logo5 from './engineering.jpg';
+import logo6 from './usp.jpg';
+import logo7 from './de.png';
+import logo8 from './science.png';
+// import {Link} from 'react-router-dom';
 import './styles.css';
 
 class ContactCard extends Component {
@@ -10,7 +18,7 @@ class ContactCard extends Component {
 
     return (
         <Card.Group>
-        <Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
           <Card.Content>
           <Card.Meta>
             <img src={logo} alt="Logo" />
@@ -19,31 +27,76 @@ class ContactCard extends Component {
           <Card.Header><a href="https://www.fassclub.org/">FASS Club</a></Card.Header>
           </Card.Content>
         </Card>
-        <Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
           <Card.Content>
           <Card.Meta>
-            <img src={logo} alt="Logo" />
+            <img src={logo1} alt="Logo" />
           </Card.Meta>
           <br></br>
-          <Card.Header><a href="https://www.fassclub.org/">FASS Club</a></Card.Header>
+          <Card.Header><a href="https://nuscomputing.com/">Computing Club</a></Card.Header>
           </Card.Content>
         </Card>
-        <Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
           <Card.Content>
           <Card.Meta>
-            <img src={logo} alt="Logo" />
+            <img src={logo2} alt="Logo" />
           </Card.Meta>
           <br></br>
-          <Card.Header><a href="https://www.fassclub.org/">FASS Club</a></Card.Header>
+          <Card.Header><a href="http://www.nusbizadclub.com/">Bizad Club</a></Card.Header>
           </Card.Content>
         </Card>
-        <Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
           <Card.Content>
           <Card.Meta>
-            <img src={logo} alt="Logo" />
+            <img src={logo3} alt="Logo" />
           </Card.Meta>
           <br></br>
-          <Card.Header><a href="https://www.fassclub.org/">FASS Club</a></Card.Header>
+          <Card.Header><a href="http://www.nusmedsoc.com/">Medical Society</a></Card.Header>
+          </Card.Content>
+        </Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
+          <Card.Content>
+          <Card.Meta>
+            <img src={logo4} alt="Logo" />
+          </Card.Meta>
+          <br></br>
+          <Card.Header><a href="https://www.nuslawclub.com/">Law Club</a></Card.Header>
+          </Card.Content>
+        </Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
+          <Card.Content>
+          <Card.Meta>
+            <img src={logo5} alt="Logo" />
+          </Card.Meta>
+          <br></br>
+          <Card.Header><a href="http://enginclub.com/">Engineering Club</a></Card.Header>
+          </Card.Content>
+        </Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
+          <Card.Content>
+          <Card.Meta>
+            <img src={logo6} alt="Logo" />
+          </Card.Meta>
+          <br></br>
+          <Card.Header><a href="https://nususc.com/">University Scholars Club</a></Card.Header>
+          </Card.Content>
+        </Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
+          <Card.Content>
+          <Card.Meta>
+            <img src={logo7} alt="Logo" />
+          </Card.Meta>
+          <br></br>
+          <Card.Header><a href="https://sdemedia.wixsite.com/declub">Design & Environment Club</a></Card.Header>
+          </Card.Content>
+        </Card>
+        <Card style={{width:"15%", height:"15%", textAlign: "center"}}>
+          <Card.Content>
+          <Card.Meta>
+            <img src={logo8} alt="Logo" />
+          </Card.Meta>
+          <br></br>
+          <Card.Header><a href="https://www.facebook.com/nus.science.club/">Science Club</a></Card.Header>
           </Card.Content>
         </Card>
         </Card.Group>
---
+++ frontend/src/components/dashboard/dashboard.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Notifications from './notifications';
+
+class Dashboard extends Component {
+    render(){
+        return(
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
